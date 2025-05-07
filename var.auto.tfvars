e8f0693909642689b194e9cdf106bbf208728fc6
vivekram123 <vivekram.techies123@gmail.com>
2025-05-07 18:15:29 +0530

    add variable and value

---
+++ var.auto.tfvars
@@ -0,0 +1,8 @@
+project_id="winter-berm-453805-n5"
+region="asia-south2"
+zone="asia-south2-a"
+subnetwork="delhi-subnet-pub"
+num_instances=1
+nat_ip=null
+network_tier="PREMIUM"
+service_account=null
---
+++ variable.tf
@@ -0,0 +1,43 @@
+variable "project_id" {
+  description = "The GCP project to use for integration tests"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region to create and test resources in"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "The GCP zone to create resources in"
+  type        = string
+  default     = null
+}
+
+variable "subnetwork" {
+  description = "The subnetwork selflink to host the compute instances in"
+}
+
+variable "num_instances" {
+  description = "Number of instances to create"
+}
+
+variable "nat_ip" {
+  description = "Public ip address"
+  default     = null
+}
+
+variable "network_tier" {
+  description = "Network network_tier"
+  default     = "PREMIUM"
+}
+
+variable "service_account" {
+  default = null
+  type = object({
+    email  = string,
+    scopes = set(string)
+  })
+  description = "Service account to attach to the instance. See https://www.terraform.io/docs/providers/google/r/compute_instance_template#service_account."
+}
